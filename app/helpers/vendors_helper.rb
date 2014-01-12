d14b46c03df9616faa73f398feccec5e7c207827
root <root@ubuntu.(none)>
2014-01-13 01:48:39 +0530

    handled division by zero for votes

--- app/helpers/vendors_helper.rb
+++ app/helpers/vendors_helper.rb
@@ -1,5 +1,5 @@
 module VendorsHelper
 	def vote_percent(vendor)
-			(vendor.positives.size * 100)/(vendor.positives.size + vendor.negatives.size)
+			(vendor.positives.size * 100)/((vendor.positives.size + vendor.negatives.size).nonzero? || 1)
 	end
 end
