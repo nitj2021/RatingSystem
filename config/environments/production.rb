76be58297b6d780ba0d843bdd6329deb10081e96
root <root@ubuntu.(none)>
2014-01-12 22:15:03 +0530

    added default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ RatingSystem::Application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
+  config.action_mailer.default_url_options = { :host => 'ratingdemo.herokuapp.com' }
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
